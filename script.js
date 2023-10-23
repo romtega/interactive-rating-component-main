b90254ef874230e601afe3cf451cc4bfbd9c8de0
Roman Ortega Mondaca <romtega88@gmail.com>
2023-10-23 15:03:39 +0200

    js working

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("hello world");
+
+const sectionRating = document.querySelector("#section-rating");
+const sectionThankyou = document.querySelector("#section-thankyou");
+const rating = document.querySelectorAll(".rating-number");
+const btnSubmit = document.querySelector("#btn-submit");
+const score = document.querySelector("#score");
+
+let rate;
+
+function openThankyou() {
+  score.innerHTML = rate;
+  sectionRating.classList.add("hidden");
+  sectionThankyou.classList.remove("hidden");
+}
+
+function closeThankyou() {
+  rate = undefined;
+  sectionRating.classList.remove("hidden");
+  sectionThankyou.classList.add("hidden");
+}
+
+for (let i = 0; i < rating.length; i++) {
+  rating[i].addEventListener("click", () => {
+    rate = rating[i].innerHTML;
+  });
+}
+
+btnSubmit.addEventListener("click", () => {
+  if (rate) {
+    openThankyou();
+  }
+});
